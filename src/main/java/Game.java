d57ece5aac80f3aadc1a1121c0bf5dc225296c10
adrian-hernandez <1hernandezadrian1@gmail.com>
2024-11-01 19:38:58 -0700

    Refactor the agent code in the Game Class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -20,34 +20,21 @@ public class Game {
     private String gem = "red-gem.png";
     private String[] enemies = {"enemy.png", "enemy2.png", "enemy3.png"};
     private String obs = "obstacle.png";
-
     private Color[] rgb = {new Color(255, 0, 0), new Color(0, 255, 0), new Color(0, 0, 255)};
     private boolean isPaused = false;
     private boolean isOver = false;
-    // TODO make the visuals look nicer
     private boolean nodeVisuals = false;
     private Grid grid;
     private int score;
     private int userRow;
     private int userCol;
     private Location userLoc;
-    private int enemyRow;
-    private int enemyCol;
     private Location gemLoc;
-    private int gemRow;
-    private int gemCol;
     private boolean gemExists;
-    private Location enemyLoc;
-    private int enemyRow2;
-    private int enemyCol2;
-    private Location enemyLoc2;
-    private int enemyRow3;
-    private int enemyCol3;
-    private Location enemyLoc3;
+    private Location[] enemyLocs = new Location[3];
     private int msElapsed;
     private int dimensions;
-    private int cut = 15; //Used in the measurement of the minimum distance to enemies
-    // For A* to execute per enemy
+    private int cut = 15;
     private int nTimesKeyHandled;
 
     public Game() {
@@ -55,93 +42,101 @@ public class Game {
         grid = new Grid(dimensions, dimensions);
         score = 0;
         addObs();
+
+        // Set up user location
         userRow = grid.getNumRows() - 1;
         userCol = 0;
         userLoc = new Location(userRow, userCol);
-        gemRow = getFreeCellForGem().getRow();
-        gemCol = getFreeCellForGem().getCol();
-        gemLoc = new Location(gemRow, gemCol);
-        enemyRow = 0;
-        enemyCol = 0;
-        enemyLoc = new Location(enemyRow, enemyCol);
-        enemyRow2 = 0;
-        enemyCol2 = grid.getNumCols() - 1;
-        enemyLoc2 = new Location(enemyRow2, enemyCol2);
-        enemyRow3 = grid.getNumRows() - 1;
-        enemyCol3 = grid.getNumCols() - 1;
-        enemyLoc3 = new Location(enemyRow3, enemyCol3);
+        grid.setImage(userLoc, user);
+
+        // Set up gem location
+        gemLoc = getFreeCellForGem();
+        grid.setImage(gemLoc, gem);
+        gemExists = true;
+
+        // Set up enemy locations
+        int[][] enemyPositions = {
+                {0, 0},
+                {0, grid.getNumCols() - 1},
+                {grid.getNumRows() - 1, grid.getNumCols() - 1}
+        };
+
+        for (int i = 0; i < enemyLocs.length; i++) {
+            enemyLocs[i] = new Location(enemyPositions[i][0], enemyPositions[i][1]);
+            grid.setImage(enemyLocs[i], enemies[i]);
+        }
+
         msElapsed = 0;
         updateTitle();
-        grid.setImage(new Location(userRow, userCol), user);
-        grid.setImage(new Location(gemRow, gemCol), gem);
-        gemExists = true;
-        grid.setImage(new Location(enemyRow, enemyCol), enemies[0]);
-        grid.setImage(new Location(enemyRow2, enemyCol2), enemies[1]);
-        grid.setImage(new Location(enemyRow3, enemyCol3), enemies[2]);
     }
 
     public void play() {
         Random r = new Random();
-        Stack<Location> tempPath;
-        Stack<Location> tempPath2;
-        Stack<Location> tempPath3;
-        Location tempUser;
+//        Stack<Location> tempPath;
+//        Stack<Location> tempPath2;
+//        Stack<Location> tempPath3;
+//        Location tempUser;
 
         // This variable helps cap the number of times the key is handled while the
         // Thread is sleeping (capping it to 1)
 
         while (!isOver) {
-            //Allows user to unpause the game
-            while(isPaused){
+            // Allows user to unpause the game
+            while (isPaused) {
                 grid.pause(50);
                 handleKeyPress();
             }
+
             while (!isPaused && !isOver) {
                 nTimesKeyHandled = 0;
                 grid.pause(100);
-                if(nTimesKeyHandled < 1){
+                if (nTimesKeyHandled < 1) {
                     handleKeyPress();
                 }
+
                 gemCaught();
                 if (!gemExists) addGem();
-                tempPath = (findPath(enemyLoc, userLoc));
-                tempPath2 = (findPath(enemyLoc2, userLoc));
-                tempPath3 = (findPath(enemyLoc3, userLoc));
-                tempUser = userLoc;
+
+                // Find paths for each enemy to the user location
+                Stack<Location>[] tempPaths = new Stack[enemyLocs.length];
+                for (int i = 0; i < enemyLocs.length; i++) {
+                    tempPaths[i] = findPath(enemyLocs[i], userLoc);
+                }
 
                 if (msElapsed % 200 == 0) {
+                    int random = r.nextInt(enemyLocs.length);
 
-                    int random = r.nextInt(3);
-                    if (!tempPath.isEmpty()) {
-                        //Randomly selects which enemy makes the first chasing move
-                        if (tempPath.peek().equals(tempPath2.peek())
-                                || tempPath.peek().equals(tempPath3.peek())) {
-                            if (random == 0) {
-                                enemyMove(tempPath.pop(), enemyLoc, enemies[0]);
-                                isGameOver();
-                            } else if (random == 1) {
-                                enemyMove(tempPath2.pop(), enemyLoc2, enemies[1]);
-                                isGameOver();
-                            } else if (random == 2) {
-                                enemyMove(tempPath3.pop(), enemyLoc3, enemies[2]);
+                    if (!tempPaths[0].isEmpty()) {
+                        // Randomly selects which enemy makes the first chasing move
+                        boolean overlap = tempPaths[0].peek().equals(tempPaths[1].peek())
+                                || tempPaths[0].peek().equals(tempPaths[2].peek());
+
+                        if (overlap) {
+                            // If there's overlap, move the randomly selected enemy first
+                            // TODO create an Enemy Class to make managing their movement easier
+                            if (!tempPaths[random].isEmpty()) {
+                                enemyMove(tempPaths[random].peek(), enemyLocs[random], enemies[random]);
+                                updateEnemyLocation(random, tempPaths[random].pop());
                                 isGameOver();
                             }
-
                         } else {
-                            enemyMove(tempPath.pop(), enemyLoc, enemies[0]);
-                            isGameOver(); //Returns true if enemy ocupies the same space as user
-                            enemyMove(tempPath2.pop(), enemyLoc2, enemies[1]);
-                            isGameOver();
-                            enemyMove(tempPath3.pop(), enemyLoc3, enemies[2]);
-                            isGameOver();
+                            // If no overlap, move all enemies one step
+                            for (int i = 0; i < enemyLocs.length; i++) {
+                                if (!tempPaths[i].isEmpty()) {
+                                    enemyMove(tempPaths[i].peek(), enemyLocs[i], enemies[i]);
+                                    updateEnemyLocation(i, tempPaths[i].pop());
+                                    isGameOver();
+                                }
+                            }
                         }
                     }
                 }
+
                 updateTitle();
                 msElapsed += 100;
-                //upCut();
             }
         }
+
     }
 
     public void handleKeyPress() {
@@ -223,8 +218,9 @@ public class Game {
         userLoc = (new Location(userRow, userCol));
     }
 
-    public void updateEnemyLocation() {
-        enemyLoc = (new Location(enemyRow, enemyCol));
+    public void updateEnemyLocation(int enemyIndex, Location newEnemyLocation) {
+        // Update the specified enemy's location based on the provided index
+        enemyLocs[enemyIndex] = newEnemyLocation;
     }
 
     // Checks if a location is within boundary
@@ -254,7 +250,6 @@ public class Game {
         grid.setImage(new Location(regi.getRow(), regi.getCol()), null);
         regi.row = closer.getRow();
         regi.col = closer.getCol();
-        // updateEnemyLocation();
         grid.setImage(new Location(regi.getRow(), regi.getCol()), which);
     }
 
@@ -439,42 +434,48 @@ public class Game {
     }
 
     public void addGem() {
-        Location newGemLoc = getFreeCellForGem();
-        gemRow = newGemLoc.getRow();
-        gemCol = newGemLoc.getCol();
-        updateGemLocation();
-        grid.setImage(new Location(newGemLoc.getRow(), newGemLoc.getCol()), gem);
+        gemLoc = getFreeCellForGem();  // Assign the new gem location directly to gemLoc
+        updateGemLocation(gemLoc);
+        grid.setImage(gemLoc, gem);    // Update the grid with the gem image at the new location
         gemExists = true;
     }
 
-    public void updateGemLocation() {
-        gemLoc = new Location(gemRow, gemCol);
+    public void updateGemLocation(Location newGemLoc) {
+        gemLoc = newGemLoc;
     }
 
     public void updateTitle() {
         grid.setTitle("Pursuit " + "Score " + getScore());
     }
 
-    public void isGameOver(){
+    public void isGameOver() {
         // The game ends once user is touched by enemy
         if (!isOver) {
-            if (enemyLoc.equals(userLoc) || enemyLoc2.equals(userLoc)
-                    || enemyLoc3.equals(userLoc)) {
+            boolean enemyTouchedUser = false;
+
+            // Check if any enemy location equals user location
+            for (Location enemyLoc : enemyLocs) {
+                if (enemyLoc.equals(userLoc)) {
+                    enemyTouchedUser = true;
+                    break; // Exit loop on first match
+                }
+            }
+
+            if (enemyTouchedUser) {
                 isOver = true;
                 System.out.println("Game Over!");
                 grid.showMessageDialog("Game Over!\n" + "Score " + getScore());
                 int choice = grid.showConfirmDialog();
-                if(choice == JOptionPane.YES_NO_OPTION){
+                if (choice == JOptionPane.YES_NO_OPTION) {
                     grid.getFrame().dispose();
                     test();
-                }
-                else{
+                } else {
                     grid.getFrame().dispose();
                 }
-
             }
         }
     }
+
 
     public int getScore() {
         return this.score + (msElapsed / 1000);
